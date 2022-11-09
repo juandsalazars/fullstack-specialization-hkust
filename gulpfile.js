ae4d4c7c208f4ea7a50b9978a0476b936d614402
Juan David Salazar Santa <juandsalazars@gmail.com>
2022-11-09 11:41:13 -0500

    Gulp Part 2 - Build usemin

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@
 
 var gulp = require('gulp'),
     gulp_sass = require('gulp-sass')(require('node-sass')),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    rimraf = require('gulp-rimraf'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
 function sass () {
   return gulp.src('./css/*.scss')
@@ -10,11 +18,12 @@ function sass () {
     .pipe(gulp.dest('./css'));
 }
 
-function sassWatch () {
+function sassWatch (cb) {
   gulp.watch('./css/*.scss', sass);
+  cb();
 }
 
-function browser_sync () {
+function browser_sync (cb) {
    var files = [
       './*.html',
       './css/*.css',
@@ -28,7 +37,49 @@ function browser_sync () {
       }
    });
 
+   cb();
 }
 
-// Default task
+function clean () {
+    return gulp.src('./dist', { read: false }) // much faster
+   .pipe(rimraf());
+}
+
+function copyfonts (cb){
+    gulp.src('./node_modules/font-awesome/fonts/**/*.*')
+    .pipe(gulp.dest('./dist/fonts'));
+    cb();
+}
+
+function image_min () {
+    return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({
+        optimizationLevel: 3,
+        progressive: true, 
+        interlaced: true
+    }))
+    .pipe(gulp.dest('dist/img'));
+}
+
+function use_min () {
+    return gulp.src('./*.html')
+    .pipe(flatmap(function(stream, file){
+        return stream
+        .pipe(usemin({
+            css: [rev()],
+            html: [ function() {
+                return htmlmin({collapseWhitespace: true})
+            }],
+            js: [uglify(), rev()],
+            inlinejs: [uglify()],
+            inlinecss: [cleanCss(), 'concat']
+        }))
+    }))
+    .pipe(gulp.dest('./dist'));
+}
+
 exports.default = gulp.parallel(sassWatch, browser_sync);
+exports.build = gulp.series(
+    clean,
+    gulp.parallel(copyfonts, image_min, use_min)
+);
